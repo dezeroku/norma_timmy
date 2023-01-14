3301a5dd6e14bae469906f00b5a2e0742cc49150
dezeroku <dezeroku@gmail.com>
2023-01-14 22:48:25 +0100

    Updates

---
+++ precache-manifest.6de463a0d58dadc49c28883b9fde7700.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "269fd947b415ec9f3b5a2654ba8d1044",
+    "url": "/norma_timmy/index.html"
+  },
+  {
+    "revision": "efc4183aebedbfa6297b",
+    "url": "/norma_timmy/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "1da7c0d7207b7d9583eb",
+    "url": "/norma_timmy/static/css/main.3c38d852.chunk.css"
+  },
+  {
+    "revision": "efc4183aebedbfa6297b",
+    "url": "/norma_timmy/static/js/2.7a3ea5f9.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/norma_timmy/static/js/2.7a3ea5f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1da7c0d7207b7d9583eb",
+    "url": "/norma_timmy/static/js/main.677b3c66.chunk.js"
+  },
+  {
+    "revision": "7a292e1fc946554c6ecc",
+    "url": "/norma_timmy/static/js/runtime-main.fec16444.js"
+  }
+]);
