9d3d53641fe522a9119d749d495af87fdaa6de1e
dezeroku <dezeroku@gmail.com>
2023-01-15 01:16:31 +0100

    remove serviceworker references

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./index.css";
 import Home from "./Home";
 import ReactDOM from "react-dom/client";
-import * as serviceWorker from "./serviceWorker";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -12,8 +11,3 @@ root.render(
     <Home />
   </React.StrictMode>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
