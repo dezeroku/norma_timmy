fb130a81514e63d521ce95fcc817e6455d7f4c78
dezeroku <dezeroku@gmail.com>
2023-01-31 23:53:01 +0100

    proper split into parent-component relationship

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,22 +3,17 @@ import React from "react";
 import { Navbar, Form } from "react-bootstrap";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import PickerComponent from "./PickerComponent";
 
 type HomeProps = {};
 
 type HomeState = {
-  fours: number;
-  eights: number;
-  twelves: number;
+  names: string[];
+  values: number[];
+  required: number[];
+  predicts: number[];
+
   summary: number;
-
-  requiredFours: number;
-  requiredEights: number;
-  requiredTwelves: number;
-
-  foursPredict: number;
-  eightsPredict: number;
-  twelvesPredict: number;
 };
 
 class Home extends React.Component<HomeProps, HomeState> {
@@ -26,71 +21,50 @@ class Home extends React.Component<HomeProps, HomeState> {
     this.recalculate();
   }
 
-  onChange(e: any) {
-    console.log(this.state.eights);
-    if (e.target.id === "fourPortsField") {
-      console.log(e);
-      if (e.target.value >= 0) {
-        this.setState({ fours: e.target.value }, this.recalculate);
-      }
-    }
-
-    if (e.target.id === "eightPortsField") {
-      console.log(e);
-      if (e.target.value >= 0) {
-        this.setState({ eights: e.target.value }, this.recalculate);
-      }
-    }
-    if (e.target.id === "twelvePortsField") {
-      console.log(e);
-      if (e.target.value >= 0) {
-        this.setState({ twelves: e.target.value }, this.recalculate);
-      }
-    }
-  }
-
-  recalculate() {
-    this.setState(
-      {
-        summary:
-          this.state.fours / this.state.requiredFours +
-          this.state.eights / this.state.requiredEights +
-          this.state.twelves / this.state.requiredTwelves,
-      },
-      this.recalculate_predicts
-    );
-  }
-
-  recalculate_predicts() {
-    this.setState({
-      twelvesPredict: (1 - this.state.summary) * this.state.requiredTwelves,
-      eightsPredict: (1 - this.state.summary) * this.state.requiredEights,
-      foursPredict: (1 - this.state.summary) * this.state.requiredFours,
-    });
-  }
-
   constructor(props: HomeProps) {
     super(props);
 
     this.state = {
-      fours: 0,
-      eights: 0,
-      twelves: 0,
+      // The whole configuration happens in the "names" and "required" lists
+      // It's important to keep all the lists with the same length
+      values: [0, 0, 0],
+      predicts: [0, 0, 0],
+      names: ["12p", "8p", "4p"],
+      required: [115, 165, 190],
+
       summary: 0,
-
-      requiredFours: 190,
-      requiredEights: 165,
-      requiredTwelves: 115,
-
-      foursPredict: 0,
-      eightsPredict: 0,
-      twelvesPredict: 0,
     };
-
-    this.onChange = this.onChange.bind(this);
-    this.recalculate = this.recalculate.bind(this);
-    this.recalculate_predicts = this.recalculate_predicts.bind(this);
   }
+
+  recalculate = () => {
+    this.setState(
+      {
+        summary: this.state.values
+          .map((k, i) => k / this.state.required[i])
+          .reduce((a, b) => a + b, 0),
+      },
+      this.recalculate_predicts
+    );
+  };
+
+  recalculate_predicts = () => {
+    this.setState({
+      predicts: this.state.required.map((k, _) =>
+        this.state.summary < 1 ? (1 - this.state.summary) * k : 0
+      ),
+    });
+  };
+
+  handleValueUpdate = (index: number, value: number) => {
+    const nextValues = this.state.values.map((c, i) => {
+      if (i === index) {
+        return value;
+      } else {
+        return c;
+      }
+    });
+    this.setState({ values: nextValues }, this.recalculate);
+  };
 
   render() {
     return (
@@ -99,49 +73,16 @@ class Home extends React.Component<HomeProps, HomeState> {
           <Navbar.Brand>Kalkulator Norm</Navbar.Brand>
         </Navbar>
         <Form>
-          <Form.Group className="mb-3" controlId="twelvePortsField">
-            <Form.Label>12p ({this.state.requiredTwelves})</Form.Label>
-            <Form.Control
-              type="number"
-              min="0"
-              step="1"
-              placeholder={this.state.twelvesPredict.toString()}
-              value={this.state.twelves === 0 ? "" : this.state.twelves}
-              onChange={this.onChange}
+          {this.state.required.map((_, i) => (
+            <PickerComponent
+              required={this.state.required[i]}
+              placeholder_fill={this.state.predicts[i].toString()}
+              updateParent={(val) => this.handleValueUpdate(i, val)}
+              name={this.state.names[i]}
+              key={i}
             />
-            <Form.Text className="text-muted">
-              {(this.state.twelves / this.state.requiredTwelves) * 100}%
-            </Form.Text>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="eightPortsField">
-            <Form.Label>8p ({this.state.requiredEights})</Form.Label>
-            <Form.Control
-              type="number"
-              min="0"
-              step="1"
-              placeholder={this.state.eightsPredict.toString()}
-              value={this.state.eights === 0 ? "" : this.state.eights}
-              onChange={this.onChange}
-            />
-            <Form.Text className="text-muted">
-              {(this.state.eights / this.state.requiredEights) * 100}%
-            </Form.Text>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="fourPortsField">
-            <Form.Label>4p ({this.state.requiredFours})</Form.Label>
-            <Form.Control
-              type="number"
-              min="0"
-              step="1"
-              placeholder={this.state.foursPredict.toString()}
-              value={this.state.fours === 0 ? "" : this.state.fours}
-              onChange={this.onChange}
-            />
-            <Form.Text className="text-muted">
-              {(this.state.fours / this.state.requiredFours) * 100}%
-            </Form.Text>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="percentageSum">
+          ))}
+          <Form.Group className="mb-3">
             <Form.Label>Suma procent</Form.Label>
             <Form.Text className="text-muted">
               {this.state.summary * 100}%
---
+++ src/PickerComponent.tsx
@@ -0,0 +1,57 @@
+import { Form } from "react-bootstrap";
+import React from "react";
+
+type PickerComponentState = {
+  current: number;
+};
+
+type PickerComponentProps = {
+  required: number;
+  placeholder_fill: string;
+  updateParent: (val: number) => void;
+
+  name: string;
+};
+
+class PickerComponent extends React.Component<
+  PickerComponentProps,
+  PickerComponentState
+> {
+  constructor(props: PickerComponentProps) {
+    super(props);
+    this.state = {
+      current: 0,
+    };
+  }
+
+  on_update = (value: number) => {
+    this.setState({
+      current: value,
+    });
+    this.props.updateParent(value);
+  };
+
+  render() {
+    return (
+      <Form.Group className="mb-3">
+        <Form.Label>
+          {this.props.name} ({this.props.required})
+        </Form.Label>
+        <Form.Control
+          type="number"
+          min="0"
+          //max="{this.props.required}"
+          step="1"
+          placeholder={this.props.placeholder_fill}
+          value={this.state.current === 0 ? "" : this.state.current}
+          onChange={(e) => this.on_update(Number(e.target.value || ""))}
+        />
+        <Form.Text className="text-muted">
+          {(this.state.current / this.props.required) * 100}%
+        </Form.Text>
+      </Form.Group>
+    );
+  }
+}
+
+export default PickerComponent;
